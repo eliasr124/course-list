8700f7e8164ba21930f8e72de9c096f3f37bef18
Elias <elias.cruz@grupompz.com.br>
2022-01-03 09:14:49 -0500

    project course angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { CourseListComponent } from './courses/course-list.component';
+import { StarComponent } from './star/star.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseListComponent,
+    StarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/courses/course-list.component.ts
@@ -0,0 +1,18 @@
+import { Component } from "@angular/core";
+import { Course } from "./course";
+import { CourseService } from "./course.service";
+
+@Component({
+    selector: 'app-course-list',
+    templateUrl: './course-list.component.html'
+})
+export class CourseListComponent{
+
+ courses: Course[] = [];
+
+    constructor(private courseService: CourseService) {}
+
+    ngOnInit(): void{
+       this.courses = this.courseService.retrieveAll();
+    }
+}
